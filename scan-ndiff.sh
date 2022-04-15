e14f7d9424fe133a98c673567c61d89c6a0a3253
Mateusz Borowski <mateusz.borowski@elitpartner.pl>
2022-04-15 13:09:17 +0200

    working changes on scan

--- scan-ndiff.sh
+++ scan-ndiff.sh
@@ -1,36 +1,34 @@
 #!/bin/sh
-TARGETS="targets.txt"
-OPTIONS="-sV -oA nmap-vuln -Pn -script vulnerability -iL "
+TARGETS="/home/Projects/red_team/targets.txt"
+OPTIONS="-sV -Pn -script vuln -iL "
+EXPORT="-oX"
 date=`date +%F`
 # METASPLOIT SCAN SCRIPT
 METASPLOIT_SCAN_SCRIPT='./metasploit_scan_script'
 
+nmap $OPTIONS $TARGETS $EXPORT scan-$date.xml
 
-cd /root/scans
-nmap $OPTIONS $TARGETS -oA scan-$date > /dev/null
+echo '*** HTML EXPORT ***'
+xsltproc scan-$date.xml -o vuln-$date.htm  
+
+echo '*** COMPARE RESULTS ***'
 if [ -e scan-prev.xml ]; then
     ndiff scan-prev.xml scan-$date.xml > diff-$date
     echo "*** NDIFF RESULTS ***"
     cat diff-$date
     echo
-fi 
-echo "*** NMAP RESULTS *** "
-cat scan-$date.nmap 
-echo '*** HTML EXPORT and CLEANING ***'
-xsltproc scan-$date.xml -o vuln-$date.html  
-ndiff scan-prev.xml scan-$date.xml --xml diff-$date.xml  
-xsltproc diff-$date.xml -o vuln_changes-$date.html  
+fi
+
+#xsltproc diff-$date.xml -o vuln_changes-$date.html  
 ln -sf scan-$date.xml scan.prev.xml 
 
 # report results in vuln-$date.html and vuln_changes-$date.html 
 
- ### METASPLOIT ###
-print_succ 'metasploit is scanning...'
+### METASPLOIT ###
+echo 'metasploit is scanning...'
         
 # start postgresql server
 service postgresql start
         
 # 
-msfconsole -q -o "$FILE_PATH/IG/metasploit_scan.txt" -x "setg rhosts $TARGETS ; resource $METASPLOIT_SCAN_SCRIPT ; exit -y"
-
-        
+msfconsole -q -o "metasploit_scan.txt" -x "setg rhosts file:$TARGETS ; resource $METASPLOIT_SCAN_SCRIPT ; exit -y"
