fde6d0fd3f511636b164c5c387fd0d81b7d6ca4d
Mateusz Borowski <mateusz.borowski@elitpartner.pl>
2022-04-15 09:38:41 +0200

    add metasploit scan

--- scan-ndiff.sh
+++ scan-ndiff.sh
@@ -2,6 +2,10 @@
 TARGETS="targets.txt"
 OPTIONS="-sV -oA nmap-vuln -Pn -script vulnerability -iL "
 date=`date +%F`
+# METASPLOIT SCAN SCRIPT
+METASPLOIT_SCAN_SCRIPT='./metasploit_scan_script'
+
+
 cd /root/scans
 nmap $OPTIONS $TARGETS -oA scan-$date > /dev/null
 if [ -e scan-prev.xml ]; then
@@ -19,3 +23,14 @@ xsltproc diff-$date.xml -o vuln_changes-$date.html
 ln -sf scan-$date.xml scan.prev.xml 
 
 # report results in vuln-$date.html and vuln_changes-$date.html 
+
+ ### METASPLOIT ###
+print_succ 'metasploit is scanning...'
+        
+# start postgresql server
+service postgresql start
+        
+# 
+msfconsole -q -o "$FILE_PATH/IG/metasploit_scan.txt" -x "setg rhosts $TARGETS ; resource $METASPLOIT_SCAN_SCRIPT ; exit -y"
+
+        
