0f44b4ca099cab7696f19905a7026fa2234aa0f9
Mateusz Borowski <mateusz.borowski@elitpartner.pl>
2022-04-19 09:30:29 +0200

    add to wpscan script result and api token

--- wpscan-auto.sh
+++ wpscan-auto.sh
@@ -1,2 +1,4 @@
 TARGET="https://site.com"
-wpscan --url $TARGET —-output file.json --format json --api-token 1m4GnIY0iLjD9cbEyYVaFBdWbSo9INvz2aTiiIVIPAQ —-random-user-agent  --enumerate u
+API_TOKEN="1m4GnIY0iLjD9cbEyYVaFBdWbSo9INvz2aTiiIVIPAQ"
+wpscan --url $TARGET —-output file.json --format json --api-token $API_TOKEN —-random-user-agent --enumerate u
+python3 -m wpscan_out_parse file.json --format html > html_output.html		#html result
