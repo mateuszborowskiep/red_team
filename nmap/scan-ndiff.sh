cebeaf4b9474fea657152c956d989c371cf43425
Mateusz Borowski <mateusz.borowski@elitpartner.pl>
2022-06-28 09:03:54 +0200

    faster scan script

--- nmap/scan-ndiff.sh
+++ nmap/scan-ndiff.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # requirement packages nmap, ndiff, metasploit-module, xsltproc
 TARGETS="./targets.txt"                                 # change this
-OPTIONS="-sV -Pn --script default,safe,intrusive -iL "  # options and categories to the script https://nmap.org/nsedoc/categories/version.html 
+OPTIONS="-sV -Pn -O -T5 -PE -F --osscan-limit --max-rtt-timeout 100ms --max-parallelism 100 --script default,vuln -iL "  # options and categories to the script https://nmap.org/nsedoc/categories/version.html 
 EXPORT="-oX"
 date=`date +%F`
 
