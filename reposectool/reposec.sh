54132d4b76ede7e4a6fda9a3f07d7a8e0f17dcbf
mateuszborowskiep <45963093+mateuszborowskiep@users.noreply.github.com>
2023-05-26 10:10:25 +0200

    changes to reposec

--- reposectool/reposec.sh
+++ reposectool/reposec.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ORG_NAME="NAME_OF_ORGANIZATION"
 
-# before that you must get authentication from github 'gh auth login' and semgrep 'semgrep login'
+# before run that script you must get authentication from github 'gh auth login' and semgrep 'semgrep login'
 
 #download all repos from organization
 gh repo list $ORG_NAME --limit 1000 | while read -r repo _; do
@@ -13,9 +13,9 @@ for d in */ ; do
     cd $d
     echo "Processing $d"
     # run gitleaks
-    gitleaks detect -v --repo-path=. --report=../$d-gitleaks-report.json
+    gitleaks detect -v . --report-path ../$d-gitleaks-report.json --redact
     # run checkov
-    checkov --directory . --output json --output-file ../$d-checkov-report.json
+    checkov --directory . -o json 
     # run semgrep
-    semgrep --config auto --output ../$d-semgrep-report.json .
+    semgrep --config auto --output --json 
 done
